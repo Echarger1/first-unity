6c2a97998f3a267799240ad6ee05255da428cabe
Echarger1 <echarger1@gmail.com>
2016-07-17 16:34:51 -0400

    first commit

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 force = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(force * speed);
+
+    }
+}
